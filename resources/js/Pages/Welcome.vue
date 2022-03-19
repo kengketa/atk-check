756ebc7c61b6170843a58b0aaecdfbe6a1522d06
Sutjapong <sutjapong@conceptreach.com>
2022-03-19 20:51:44 +0700

    refactor: Refactor register global component

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -11,13 +11,11 @@
 <script>
 import {defineComponent} from 'vue'
 import {Head, Link} from '@inertiajs/inertia-vue3';
-import Layout from "@/Layouts/Layout";
 
 export default defineComponent({
   components: {
     Head,
     Link,
-    Layout
   },
   props: {
     canLogin: Boolean,
