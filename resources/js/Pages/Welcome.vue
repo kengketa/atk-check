b406eb1ec3313239d1ebadde5e4f7ce36093b4ce
Sutjapong <sutjapong@conceptreach.com>
2022-03-19 19:59:13 +0700

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-gray-200 min-h-screen">
+    <div class="w-full h-screen flex items-center justify-center">
+      <p class="text-xl text-green-800 font-bold">ATK CHECK SYSTEM</p>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import {defineComponent} from 'vue'
+import {Head, Link} from '@inertiajs/inertia-vue3';
+
+export default defineComponent({
+  components: {
+    Head,
+    Link,
+  },
+  props: {
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+  }
+})
+</script>
