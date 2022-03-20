ab747f8900e5fa011084bf0aa11150d4ad3ca0ee
Sutjapong <sutjapong@conceptreach.com>
2022-03-20 14:52:35 +0700

    refactor: Refactor responsive nav

---
+++ resources/js/Components/NavBar.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav
+    class="z-100 bg-gray-200 fixed top-0 w-full h-20 flex justify-between items-center px-4">
+    <div class="flex w-48 justify-left">
+      <button class="button" @click="$emit('navClick')">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+import {Link} from '@inertiajs/inertia-vue3';
+import vClickOutside from 'click-outside-vue3'
+
+
+export default defineComponent({
+  name: "HeadNavBar",
+  directives: {
+    clickOutside: vClickOutside.directive
+  },
+  components: {
+    Link, vClickOutside
+  },
+  mounted() {
+
+  },
+  data() {
+    return {
+      showOptionCard: false,
+      showProfileModal: false,
+      alert: {
+        show: false,
+        title: "",
+        subTitle: "",
+        icon: ""
+      }
+    }
+  },
+  methods: {
+    onClickOutsideMenuOption(event) {
+      this.showOptionCard = false;
+    },
+    updatedProfile() {
+      this.alert.title = "อัพเดท profile";
+      this.alert.subTitle = `อัพเดท profileสำเร็จ`;
+      this.alert.icon = "images/deposit_icon.png";
+      this.alert.show = true;
+    }
+  }
+})
+</script>
+<style>
+.z-100 {
+  z-index: 80;
+}
+</style>
---
+++ resources/js/Components/SideBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav
+    class="z-100 bg-gray-300 w-60 h-screen fixed left-0 top-20 px-4 transition translate ease-in-out overflow-y-auto scrollbar-hide"
+    :class="computedSideBar"
+  >
+    <div class="grid grid-cols-1 gap-2 mt-10 mb-28">
+      <Link>sdfdsfhsdj</Link>
+      <Link>sdfdsfhsdj</Link>
+      <Link>sdfdsfhsdj</Link>
+      <Link>sdfdsfhsdj</Link>
+    </div>
+  </nav>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+import {Link} from '@inertiajs/inertia-vue3';
+
+export default defineComponent({
+  name: "SideBar",
+  components: {
+    Link,
+  },
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    },
+  },
+  data() {
+    return {
+      demoMode: false
+    }
+  },
+  mounted() {
+    const url = window.location.href;
+    if (url.includes('demo')) {
+      this.demoMode = true;
+    }
+  },
+  methods: {
+    isUrl(...urls) {
+      return urls.some((url) => route().current(url));
+    },
+  },
+  computed: {
+    computedSideBar() {
+      if (this.show === true) {
+        return 'translate-x-0';
+      }
+      if (this.show === false) {
+        return '-translate-x-60'
+      }
+      //return '-translate-x-60 md:-translate-x-0'
+    },
+  }
+})
+</script>
+<style>
+.z-100 {
+  z-index: 80;
+}
+</style>
