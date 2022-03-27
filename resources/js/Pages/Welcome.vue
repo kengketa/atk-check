3dcd3aa2ad114e24660655c5da28d82ff07ad94e
NiceDay202 Omghhh <woraneti19@gmail.com>
2022-03-27 15:09:15 +0700

    feat update

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,172 +1,64 @@
 <template>
   <layout>
-    <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="relative bg-white overflow-hidden">
-  <div class="max-w-7xl mx-auto">
-    <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-      <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-        <polygon points="50,0 100,0 50,100 0,100" />
-      </svg>
-
-      <div>
-        <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
-          <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-            <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-              <div class="flex items-center justify-between w-full md:w-auto">
-                <a href="#">
-                  <span class="sr-only">Workflow</span>
-                  <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg">
-                </a>
-                <div class="-mr-2 flex items-center md:hidden">
-                  <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <!-- Heroicon name: outline/menu -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-              <a href="#" class="font-medium text-gray-500 hover:text-gray-900">ผลตรวจ</a>
-
-              <a href="#" class="font-medium text-gray-500 hover:text-gray-900">จำนวนนักเรียน</a>
-
-              <a href="#" class="font-medium text-gray-500 hover:text-gray-900">ผลตรวจครู</a>
-
-              <a href="#" class="font-medium text-gray-500 hover:text-gray-900">จำนวนครู</a>
-
-              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">จำนวนนักเรียน</a>
-            </div>
-          </nav>
-        </div>
-
-        <!--
-          Mobile menu, show/hide based on menu open state.
-
-          Entering: "duration-150 ease-out"
-            From: "opacity-0 scale-95"
-            To: "opacity-100 scale-100"
-          Leaving: "duration-100 ease-in"
-            From: "opacity-100 scale-100"
-            To: "opacity-0 scale-95"
-        -->
-        <div class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-          <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-            <div class="px-5 pt-4 flex items-center justify-between">
-              <div>
-                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
-              </div>
-              <div class="-mr-2">
-                <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                  <span class="sr-only">Close main menu</span>
-                  <!-- Heroicon name: outline/x -->
-                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-                </button>
-              </div>
-            </div>
-            <div class="px-2 pt-2 pb-3 space-y-1">
-              <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Product</a>
-
-              <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Features</a>
-
-              <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Marketplace</a>
-
-              <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
-            </div>
-            <a href="#" class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"> Log in </a>
-          </div>
-        </div>
+    <label for="price" class="block text-sm font-medium text-gray-700 mt-4">ค้นหา </label>
+    <div>
+  <div class="mt-4 rounded-md">
+    <div class="relative">
+      <input type="text" name="price" id="price" class="text-xl text-gray-700 focus:ring-indigo-500 focus:border-indigo-500 block w-1/3 px-10 rounded-md" ค้นหา="">
+      <div class="text-gray-400 text-gray-400 w-10 absolute top-0 py-3 pl-3">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        </svg>
       </div>
-
-      <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-        <div class="sm:text-center lg:text-left">
-          <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span class="block xl:inline">ATK</span>
-            <span class="block text-indigo-600 xl:inline">-Check</span>
-          </h1>
-          <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">ผลตรวจของนักเรียนเเละคุณครูที่จะมาโรงเรียนภายในวันที่30/30/90</p>
-          <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-            <div class="rounded-md shadow">
-              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"> ผลตรวจครู </a>
-            </div>
-            <div class="mt-3 sm:mt-0 sm:ml-3">
-              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"> ผลตรวจนักเรียน </a>
-            </div>
-          </div>
-        </div>
-      </main>
     </div>
   </div>
-  <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-    <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://s.isanook.com/he/0/ud/6/32277/atk-2.jpg" alt="">
+</div>
+<div class="bg-white px-4 py-3 flex items-center justify-between border-gray-200 sm:px-6">
+  <div class="flex-1 flex justify-between sm:hidden">
+    <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> Previous </a>
+    <a href="#" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> Next </a>
+  </div>
+  <div class="hidden sm:flex-1 sm:flex sm:justify-between mt-6">
+    <div class="relative flex gap-4">
+      <div
+        @click="toggleShow()"
+        class="border border-gray-500 w-16 h-10 flex justify-center items-center bg-gray-200
+        rounded-md shadow shadow-md cursor-pointer hover:bg-gray-500 transition duration-500 transform ease-in-out hover:scale-90 hover:text-white">
+        <p>{{ selectedMonth.id == 0 ? 'month' :selectedMonth.name }}</p>
+      </div>
+      <div class="flex flex-wrap gap-1 w-full" v-if="selectedMonth.id != 0">
+        <div v-for="index in selectedMonth.numberOfDays" :key="index" class="border border-gray-500 flex justify-center items-center px-4 py-2 rounded-md cursor-pointer hover:bg-gray-200 transition transform hover:scale-110">{{index}}</div>
+      </div>
+      <div v-if="showMonthList" class="absolute top-10 z-50" id="month-group">
+        <div
+          v-for="(month,index) in months" :key="index"
+          @click="selectMonth(month)"
+          class="border border-gray-500 w-16 h-10 flex justify-center items-center bg-gray-200
+          rounded-md shadow shadow-md cursor-pointer hover:bg-gray-500">
+          <p>{{ month.name }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
-    <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="bg-white shadow overflow-hidden sm:rounded-lg">
-  <div class="px-4 py-5 sm:px-6">
-    <h3 class="text-lg leading-6 font-medium text-gray-900">ข้อมูลผู้ทำเว็บนี้ATK-Check</h3>
-    <p class="mt-1 max-w-2xl text-sm text-gray-500">ข้อมูลเกี่ยวกับผลตรวจatkของนักเรียนเเละผู้ปกครอง</p>
-  </div>
-  <div class="border-t border-gray-200">
-    <dl>
-      <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">จัดทำโดย</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">witthayut phicharanan</dd>
-      </div>
-      <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">อายุ</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">16</dd>
-      </div>
-      <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">Email address</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">witthayut2548@gmail.com</dd>
-      </div>
-      <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">ศึกษาที่โรงเรียน</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">มอ.ว.</dd>
-      </div>
-      <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">About</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">ข้อมูลจะเกี่ยวกับผลตรวจatkของผู้ปกครองเเละนักเรียนที่จะมาโรงเรียนในวันที่30/30/70โดยผู้ปกครองต้องส่งข้อมูลมาให้เราด้วย</dd>
-      </div>
-      <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">ผลงานเเละประวัติผู้ทำ</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          <ul role="list" class="border border-gray-200 rounded-md divide-y divide-gray-200">
-            <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-              <div class="w-0 flex-1 flex items-center">
-                <!-- Heroicon name: solid/paper-clip -->
-                <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
-                </svg>
-                <span class="ml-2 flex-1 w-0 truncate"> ประวัติผู้ทำ </span>
-              </div>
-              <div class="ml-4 flex-shrink-0">
-                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Download </a>
-              </div>
-            </li>
-            <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-              <div class="w-0 flex-1 flex items-center">
-                <!-- Heroicon name: solid/paper-clip -->
-                <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
-                </svg>
-                <span class="ml-2 flex-1 w-0 truncate"> ผลงานต่าง </span>
-              </div>
-              <div class="ml-4 flex-shrink-0">
-                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Download </a>
-              </div>
-            </li>
-          </ul>
-        </dd>
-      </div>
-    </dl>
-  </div>
+<h2 class="text-2xl font-extrabold text-gray-900 mt-8">Atkcheck</h2>
+<div class="flex gap-4 flex-wrap">
+  <div v-for="(atkTest,index) in atkTests" :key="index">
+    <div class="w-60 relative rounded-md overflow-hidden">
+        <img class="w-full object-cover" :src="atkTest.image" alt="">
+        <div class="flex justify-center bg-gray-200 py-2">
+          <p>{{atkTest.studentName}}</p>
+        </div>
+        <div class="absolute top-0 right-0 bg-gray-200 w-6 h-6 flex justify-center items-center">
+            <p class="text-sm">1</p>
+        </div>
+    </div>
+   </div>
 </div>
+
+
+
+
   </layout>
 </template>
 
@@ -183,11 +75,49 @@ export default defineComponent({
     Head,
     Link,
   },
-  props: {
-    canLogin: Boolean,
-    canRegister: Boolean,
-    laravelVersion: String,
-    phpVersion: String,
+  data(){
+    return{
+      showMonthList:false,
+      selectedMonth: {id:0,name:'None',numberOfDays:0},
+       months: [
+         {id:0,name:'None',numberOfDays:0},
+         {id:1,name:'Jan',numberOfDays:31},
+         {id:2,name:'Feb',numberOfDays:29},
+         {id:3,name:'Mar',numberOfDays:31},
+         {id:4,name:'Apr',numberOfDays:30},
+         {id:5,name:'May',numberOfDays:31},
+         {id:6,name:'Jun',numberOfDays:30},
+         {id:7,name:'Jul',numberOfDays:31},
+         {id:8,name:'Aug',numberOfDays:31},
+         {id:9,name:'Sep',numberOfDays:30},
+         {id:10,name:'Oct',numberOfDays:31},
+         {id:11,name:'Nov',numberOfDays:30},
+         {id:12,name:'DecP',numberOfDays:31},
+       ],
+       atkTests:[
+         {id:1,studentName:'Name 1',date:'10/1/2022',image:'https://www.sikarin.com/wp-content/uploads/2021/08/3813357-1024x683.jpg'},
+         {id:2,studentName:'Name 2',date:'11/1/2022',image:'https://www.sikarin.com/wp-content/uploads/2021/08/3813357-1024x683.jpg'},
+         {id:3,studentName:'Name 3',date:'12/1/2022',image:'https://www.sikarin.com/wp-content/uploads/2021/08/3813357-1024x683.jpg'},
+         {id:4,studentName:'Name 4',date:'13/1/2022',image:'https://www.sikarin.com/wp-content/uploads/2021/08/3813357-1024x683.jpg'},
+         {id:5,studentName:'Name 5',date:'14/1/2022',image:'https://www.sikarin.com/wp-content/uploads/2021/08/3813357-1024x683.jpg'},
+         {id:6,studentName:'Name 6',date:'15/1/2022',image:'https://www.sikarin.com/wp-content/uploads/2021/08/3813357-1024x683.jpg'},
+]
+    }
+  },
+  methods:{
+    toggleShow(){
+      if(this.showMonthList == false){
+       return this.showMonthList=true;
+      }
+      if(this.showMonthList == true){
+        return  this.showMonthList=false;
+      }
+    },
+    selectMonth(month){
+      console.log(month);
+      this.selectedMonth = month;
+      return this.showMonthList = false;
+    }
   }
 })
 </script>
